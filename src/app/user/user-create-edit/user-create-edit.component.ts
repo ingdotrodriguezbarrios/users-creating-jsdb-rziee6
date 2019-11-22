d4582823b82d507dee73531dd58b534fb7f0d03e
ingdotrodriguezbarrios <ing.rodriguezbarrios@gmail.com>
2019-11-21 22:47:11 -0500

    3 files modified

--- src/app/user/user-create-edit/user-create-edit.component.ts
+++ src/app/user/user-create-edit/user-create-edit.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from './../../services/user.service';
+import { User } from './../../model/user';
+
 
 @Component({
   selector: 'app-user-create-edit',
   templateUrl: './user-create-edit.component.html',
   styleUrls: ['./user-create-edit.component.css']
 })
-export class UserCreateEditComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class UserCreateEditComponent {
+  userCreationForm;
+  constructor(private userService:UserService, private formBuilder: FormBuilder,private router: Router) { 
+    this.userCreationForm = this.formBuilder.group({
+      id:0,
+      name:'',
+      email:''
+    });
   }
 
+  async onSubmit(user : User){
+    let response :string = await this.userService.add();
+    if(response === "success"){
+      this.router.navigate(['/','success'])
+    } else {
+      this.showError();
+    }
+  }
+
+  showError() {
+    alert('Error al guardar el usuario');
+  }
 }
